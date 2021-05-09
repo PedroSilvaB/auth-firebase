f9fec015d58ef57d4c425b77c09809923262547d
Pedro Silva <55249637+PedroSilvaB@users.noreply.github.com>
2021-05-09 17:20:45 -0300

    cookies api

--- src/pages/api/cookies.ts
+++ src/pages/api/cookies.ts
@@ -6,7 +6,19 @@ import { setCookie } from '../../utils/cookies'
 const handler = (req: NextApiRequest, res: NextApiResponse) => {
   // Calling our pure function using the `res` object, it will add the `set-cookie` header
   //__Secure HTTPS
-  res.setHeader("Set-Cookie", "_Secure-TK='api-middleware!';path=/;domain=localhost;Max-Age=2600000;HttpOnly=true;SameSite=None;secure=true;priority=High;")
+  //res.setHeader("Set-Cookie", "_Secure-TK='api-middleware!';path=/;domain=localhost;Max-Age=2600000;HttpOnly=true;SameSite=None;secure=true;priority=High;")
+  const date: Date = new Date()
+  console.log(req.cookies)
+  date.setMonth(date.getMonth() + 1)
+  setCookie(res, "auth", "verdade", {
+    secure: true,
+    domain: "localhost",
+    sameSite: "strict",
+    httpOnly: true,
+    path: "/",
+    expires: date
+  })
+  req.cookies
   // Return the `set-cookie` header so we can display it in the browser and show that it works!
   /* res.end(res.getHeader('Set-Cookie')) */
   res.status(200).json([])
--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -1,14 +1,16 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from "next"
 
-const api = async (req: NextApiRequest, res: NextApiResponse) =>{
+const api = async (req: NextApiRequest, res: NextApiResponse) => {
     switch (req.method) {
         case "GET":
+            res.setHeader("Set-Cookie", "_Secure-TK='api-middleware!';path=/;domain=localhost;Max-Age=2600000;HttpOnly=true;SameSite=None;secure=true;priority=High;")
             res.status(200)
+            console.log(JSON.stringify(req.cookies).match(/_Secure-TK/g))
             res.json({})
             break;
-    
+
         default:
-            res.status(401).json({message: "Method not allowed"})
+            res.status(401).json({ message: "Method not allowed" })
             break;
     }
 }
